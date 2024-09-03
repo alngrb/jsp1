2f9e7e4d161bac7341743bc4182086d701e3fae3
alngrb <alenagorbunova163@gmail.com>
2024-09-03 15:51:21 +0300

    pr1

---
+++ assets/js/alert.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+
+alert('Я JavaScript');
+
+let name = prompt('Введите ваше имя', 'user');
+
+let surname = prompt('Введите ваше фамилию', 'surname');
+
+let otch = prompt('Введите ваше отчество', 'otch');
+
+alert(`Ваше фио ${name} ${surname} ${otch}`);
+
+
+let isTrue = confirm('Это действительно ваше фио?');
+alert(isTrue);
+
